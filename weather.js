875e3497b40704b5eeb09d8d51784fd164e8db7c
friedegg818 <double715@naver.com>
2020-10-27 01:27:27 +0900

    날씨 정보

--- weather.js
+++ weather.js
@@ -1,25 +1,28 @@
 const API_KEY = "92014bb042cf34f71371c60e82477360";
 const COORDS = "coords";
 
+// open API 이용 
 function getWeather(lat, lng) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
 }
 
+// localStorage에 위치 저장
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
+// 위치 가져오기 성공 
 function handleGeoSucces(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coordsObj = {
-        latitude,
-        longitude
+        latitude, longitude
     };
     saveCoords(coordsObj);
     getWeather(latitude, longitude)
 }
 
+// 위치 가져오기 실패 
 function handleGeoError() {
     console.log("Can't access geo location");
 }
@@ -28,11 +31,13 @@ function askForCoords() {
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
 }
 
+// 위치 정보 가져오기 
 function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
     if (loadedCoords === null) {
         askForCoords();
     } else {
+        // 위치 정보가 설정 되어 있으면 그 곳에 맞는 날씨 가져오기 
         const parsedCoords = JSON.parse(loadedCoords)
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
